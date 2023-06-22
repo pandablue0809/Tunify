391f6df5548c01794c38209d8dce4893af217f0c
pandablue0809 <bluep0415@gmail.com>
2023-06-22 20:56:26 +0530

    Sidebar library list component

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  return <div>Header</div>;
+};
+
+export default Header;
