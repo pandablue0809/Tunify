5754b8d2ac5a65bf05a737685f646a141ad2115e
pandablue0809 <bluep0415@gmail.com>
2023-06-24 23:57:20 +0530

    Create header component with back and forward router

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -33,6 +34,11 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
           >
             <RxCaretRight size={35} className="text-white" />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button>
+            <HiHome />
           </button>
         </div>
       </div>
