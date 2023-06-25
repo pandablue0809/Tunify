eafec0f0744fa990709c9011a2a39bb5d30f94cb
pandablue0809 <bluep0415@gmail.com>
2023-06-25 11:44:47 +0530

    Implement liked songs play button

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,9 @@ import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button"
+
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -37,11 +40,46 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           </button>
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
-          <button>
-            <HiHome />
+          <button className="rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition">
+            <HiHome size={20} className="text-black" />
+          </button>
+          <button className="rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition">
+            <BiSearch size={20} className="text-black" />
           </button>
         </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button className="bg-transparent text-neutral-300 font-medium" onClick={() => {}}>
+                Sign Up
+              </Button>
+            </div>
+
+            <div>
+              <Button className="bg-white px-6 py-2" onClick={() => {}}>
+                Log In
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
