ae7e13282fa674deedcef5c3b4ebc57e7cf2da1c
pandablue0809 <bluep0415@gmail.com>
2023-07-01 23:57:54 +0530

    Create modal provider and auth modal

---
+++ components/AuthModal.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Modal from "./Modal";
+
+const AuthModal = () => {
+  return (
+    <Modal
+      title="Welcome back"
+      description="Login to your account"
+      isOpen
+      onChange={() => {}}
+    ></Modal>
+  );
+};
+
+export default AuthModal;
